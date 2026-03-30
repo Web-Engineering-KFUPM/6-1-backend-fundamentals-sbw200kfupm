f8ca066cc8b9a1cf3a80744dc1569d0344b253fd
sbw200kfupm <s202337730@kfupm.edu.sa>
2026-03-30 11:53:01 +0300

    todo 1

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,13 +1,14 @@
 // TODO 1: Import Express
-
+import express from 'express';
 
 
 // TODO 2: Create the Express app and store it in a variable named app
-
-
+const app = express();
+const PORT = 3000;
 
 // TODO 3: Allow React to access the server
-
+import cors from 'cors';
+app.use(cors());
 
 
 // TODO 5: Create the home route "/"
